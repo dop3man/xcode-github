691e4c99487e95ce23a1724ad062a2561aa41290
Roman Yemtsev <rtype@Romans-MacBook-Pro.local>
2017-10-26 11:08:20 +1000

    Initial Commit

---
+++ GitHub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHub
+//
+//  Created by Roman Yemtsev on 10/26/17.
+//  Copyright © 2017 Roman Yemtsev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
